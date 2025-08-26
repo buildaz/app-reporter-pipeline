ad5f57db07c8c795574e45109897ac6fd4cddd22
SymonBezerra <simaobezerratranslate@gmail.com>
2025-08-26 11:51:57 -0300

    local deploy command impl successfully

--- data_jobs/android_ingestion/deploy.sh
+++ data_jobs/android_ingestion/deploy.sh
@@ -1,5 +1,5 @@
 docker build -t app-reporter-android-ingestion:latest .
-docker tag app-reporter-android-ingestion:latest gcr.io/site-reporter-436515/app-reporter-android-ingestion:latest
-docker push gcr.io/site-reporter-436515/app-reporter-android-ingestion:latest --location=southamerica-east1
+docker tag app-reporter-android-ingestion:latest southamerica-east1-docker.pkg.dev/site-reporter-436515/app-reporter-artifacts/app-reporter-android-ingestion:latest
+docker push southamerica-east1-docker.pkg.dev/site-reporter-436515/app-reporter-artifacts/app-reporter-android-ingestion:latest
 
-# gcloud run jobs replace job.yaml
+envsubst < job.yaml | gcloud run jobs replace -
